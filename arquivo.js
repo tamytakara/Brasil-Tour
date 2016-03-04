d057ced6bc6a25e9e0cdc1565040692f1aa11768
brosquinha <brosquinha@users.noreply.github.com>
2016-03-03 22:42:31 -0300

    Uploading files #8
    Oitavo upload dos arquivos do projeto original

---
+++ arquivo.js
@@ -0,0 +1,54 @@
+{"comentarios":[
+	 {
+        	 "nome": "Tamy Takara Yatsu",
+        	 "email": "claudiawerhmuller@gmail.com",
+        	 "estado":"saoPaulo",
+        	 "comentario":"São Paulo é uma cidade muito bonita!"
+        }, {
+       	 "nome": "Tainá Turella",
+    	 "email": "claudiawerhmuller@gmail.com",
+    	 "estado":"saoPaulo",
+    	 "comentario":"São Paulo é muito agitado!"
+    }, {
+       	 "nome": "Marina Contador",
+    	 "email": "claudiawerhmuller@gmail.com",
+    	 "estado":"saoPaulo",
+    	 "comentario":"São Paulo é legal, mas Bariri é melhor"
+    }, {
+       	 "nome": "Mateus Brigalante",
+    	 "email": "claudiawerhmuller@gmail.com",
+    	 "estado":"saoPaulo",
+    	 "comentario":"Divertido"
+    }, {
+       	 "nome": "Victor Silva",
+    	 "email": "claudiawerhmuller@gmail.com",
+    	 "estado":"saoPaulo",
+    	 "comentario":"Não sei o que comentar"
+    },{
+	   	 "nome": "Tamy Takara Yatsu",
+		 "email": "claudiawerhmuller@gmail.com",
+		 "estado":"amazonas",
+		 "comentario":"Amazonas é legal"
+	}, {
+		 "nome": "Tainá Turella",
+	 "email": "claudiawerhmuller@gmail.com",
+	 "estado":"amazonas",
+	 "comentario":"Amazonas é muito divertido!"
+	}, {
+		 "nome": "Marina Contador",
+	 "email": "claudiawerhmuller@gmail.com",
+	 "estado":"amazonas",
+	 "comentario":"Amazonas é legal, mas Bariri é melhor"
+	}, {
+		 "nome": "Mateus Brigalante",
+	 "email": "claudiawerhmuller@gmail.com",
+	 "estado":"amazonas",
+	 "comentario":"Esquisito"
+	}, {
+		 "nome": "Victor Silva",
+	 "email": "claudiawerhmuller@gmail.com",
+	 "estado":"amazonas",
+	 "comentario":"Amazonas é muito legal"
+	}
+ ]
+}
